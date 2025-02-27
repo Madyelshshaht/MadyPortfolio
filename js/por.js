477c7d712964167317cdc89df6dc84765dd87ba2
Mady <madylabtop@gmail.com>
2025-02-27 14:15:06 +0200

    add Files

---
+++ js/por.js
@@ -0,0 +1,46 @@
+var typed = new Typed('.multiple-text', {
+  strings: ['FrontEnd Developer', 'Age:20', 'FCIS || MU'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  typeDelay: 100,
+  loop: true
+});
+
+let btn_scroll = document.getElementById('scroll-btn');
+function up() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+
+window.onscroll = function () {
+  if (scrollY >= 855) {
+    btn_scroll.style.display = "Block";
+  }
+  else if (scrollY < 855) {
+    btn_scroll.style.display = "none";
+  }
+}
+
+document.addEventListener("scroll", function () {
+  let elements = document.querySelectorAll("#html, #css, #js, #boot , #communication ,#Management ,#TM  , #C");
+  let scrollThreshold = window.innerWidth <= 768 ? 750 : 450;
+  if (window.scrollY >= scrollThreshold) {
+    elements.forEach((el) => el.classList.add("animate"));
+  }
+  else if (window.scrollY < 450)
+    elements.forEach((el) => el.classList.remove("animate"));
+});
+
+
+let D_Cv = document.getElementById('D_Cv');
+
+D_Cv.onclick = function () {
+  window.alert("Download Completed 100%")
+}
+
+
+
+
